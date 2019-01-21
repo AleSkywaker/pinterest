8b75cd8f43445c08992eff8eae30aef0a00b99dd
Alex Colombo <uricaine@hotmail.com>
2019-01-21 19:39:30 +0100

    starting routes

---
+++ src/routes/index.js
@@ -0,0 +1,13 @@
+const { Router } = require('express');
+const router = Router()
+
+router.get('/', (req,res)=>{
+   res.send('Hola Alex')
+})
+router.get('/alex', (req,res)=>{
+   res.json({message: 'Hola Alex'})
+})
+
+
+module.exports = router;
+
