4c86588d44c69a143b44db90057e82b87c2357a9
Alex Colombo <uricaine@hotmail.com>
2019-01-26 23:01:01 +0100

    init route

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,7 +2,7 @@ const { Router } = require('express');
 const router = Router()
 
 router.get('/', (req,res)=>{
-   res.send('Hola Alex Colombo')
+   res.status(200).send('Hola Alex Colombo')
 })
 
 router.get('/alex', (req,res)=>{
