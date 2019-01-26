e05ef705f9d26f60104b29ebc30ec2c3c153cc44
Alex Colombo <uricaine@hotmail.com>
2019-01-26 22:59:57 +0100

    init route

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ const express = require('express')
 const path = require('path')
 const morgan = require('morgan')
 const multer = require('multer')
-
 //initialization
 const app = express();
 //Settings
